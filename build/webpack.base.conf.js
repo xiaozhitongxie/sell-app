e5d37694694b60dfee3dee05e1ea8a84e36ec358
xiaozhijun <401972401@qq.com>
2018-04-06 03:08:34 +0800

    添加了基于vue2.5的配置更改和基本数据的添加

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,15 +27,19 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath:
+      process.env.NODE_ENV === 'production'
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      vue$: 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      src: path.resolve(__dirname, '../src'),
+      common: path.resolve(__dirname, '../src/common'),
+      components: path.resolve(__dirname, '../src/components')
     }
   },
   module: {
@@ -49,7 +53,11 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [
+          resolve('src'),
+          resolve('test'),
+          resolve('node_modules/webpack-dev-server/client')
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
